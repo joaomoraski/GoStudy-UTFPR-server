83e682e4dad72034adfde93680af96e1b45bf03c
Joao Moraski <joaomoraskilunkes@gmail.com>
2022-05-18 20:36:43 -0300

    implementando rotas de teste pras migrations e seeders e update no .env-example

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,75 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { Router } from 'express'
+import { connection } from './connectDB'
+
 
 const router = Router();
 
 
-router.post('/users', (req, res) => {
-    return res.status(201).send();
+router.get('/institutes', (req, res) => {
+    (async function connectionDb(){
+        try {
+            const resultado = await connection.query("select * from users");
+            return res.status(201).send(resultado[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+})
+
+router.get('/users', (req, res) => {
+    (async function connectionDb(){
+        try {
+            const resultado = await connection.query("select * from users");
+            return res.status(201).send(resultado[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+})
+
+router.get('/rooms', (req, res) => {
+    (async function connectionDb(){
+        try {
+            const resultado = await connection.query("select * from users");
+            return res.status(201).send(resultado[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+})
+
+router.get('/schedules', (req, res) => {
+    (async function connectionDb(){
+        try {
+            const resultado = await connection.query("select * from users");
+            return res.status(201).send(resultado[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+})
+
+router.get('/rooms_has_schedules', (req, res) => {
+    (async function connectionDb(){
+        try {
+            const resultado = await connection.query("select * from users");
+            return res.status(201).send(resultado[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
+})
+
+router.get('/reservations', (req, res) => {
+    (async function connectionDb(){
+        try {
+            const resultado = await connection.query("select * from users");
+            return res.status(201).send(resultado[0]);
+        } catch (error) {
+            console.log(error);
+        }
+    })();
 })
 
 export { router }
