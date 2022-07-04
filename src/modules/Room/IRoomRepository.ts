d024623b97bfa05311c40a88b3613c6f7647098d
VinterCardoso <34047087+VinterCardoso@users.noreply.github.com>
2022-07-03 23:24:58 -0300

    Modifica Salas

--- src/modules/Room/IRoomRepository.ts
+++ src/modules/Room/IRoomRepository.ts
@@ -3,6 +3,7 @@ import { Room } from "../../entities/Room";
 interface IRoomRepository {
     create(Room: Room): Promise<Room>;
     listAllRooms(): Promise<Room[]>;
+    listByInstitute(instituteId: string): Promise<Room[]>;
     findById(id: string): Promise<Room>;
     update(Room: Room): Promise<Room>;
     delete(Room: Room): Promise<Room>;
